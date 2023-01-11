6f90c355bfd6942605c5c9e3dc9359d2d8bd549b
He Xiang Koh <kohhxx6@gmail.com>
2023-01-11 22:18:30 +0800

    passport setup 1

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const express = require('express');
+const express = require("express");
+require("dotenv").config();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
 
-app.get('/', (req, res) => {``
-  res.send({ hi: "Hello"});
-})
+// Passport Config
+passport.use(
+  new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENTID,
+    clientSecret: process.env.GOOGLE_SECRETKEY,
+    callbackURL: "/auth/google/callback",
+  },
+  (accessToken) => {
+    console.log(accessToken);
+  })
+);
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server Started...")
-})
+  console.log("Server Started...");
+});
